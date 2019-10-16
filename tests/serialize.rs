e3f39e29a84346311a571937a48936ba2b079431
Stefan Battiston <stefan.battiston@gmail.com>
2019-10-16 13:10:46 -0300

    Webassembly does not implement Time sys call. Must use web_sys.

--- tests/serialize.rs
+++ tests/serialize.rs
@@ -4,9 +4,9 @@ extern crate utilities;
 extern crate webassembly_marker_clusterer;
 
 use wasm_bindgen_test::*;
-use utilities::{ get_sample_markers };
 use wasm_bindgen::prelude::JsValue;
-use webassembly_marker_clusterer::structs::marker::Marker;
+use utilities::{ get_sample_markers };
+// use webassembly_marker_clusterer::structs::marker::Marker;
 
 // This runs a unit test in the browser, so it can use browser APIs.
 // wasm_bindgen_test_configure!(run_in_browser);
@@ -14,7 +14,16 @@ use webassembly_marker_clusterer::structs::marker::Marker;
 #[wasm_bindgen_test]
 fn serialize_deserialize() {
   let markers = get_sample_markers();
-  let js_value = JsValue::from_serde(&markers).unwrap();
-  let ser_markers: Vec<Marker> = js_value.into_serde().unwrap();
-  assert_eq!(markers.len(), ser_markers.len());
+  // let js_value = JsValue::from_serde(&markers).unwrap();
+
+  // microbench::bench(&options, "serialize/serde-default", || JsValue::from_serde(&markers).unwrap());
+  // microbench::bench(&options, "deserialize/serde-default", || -> Vec<Marker> { js_value.into_serde().unwrap() });
+
+  // println!("reverse: {}", easybench::bench(|| JsValue::from_serde(&markers).unwrap()));
+
+  // let bench_result = benchmarking::bench_function(move |measurer| {
+  //   measurer.measure(|| JsValue::from_serde(&markers).unwrap())
+  // }).unwrap();
+
+  // println!("{:?}", bench_result.elapsed());
 }
