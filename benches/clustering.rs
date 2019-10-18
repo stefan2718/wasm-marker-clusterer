4c592af13bcd739c2383b4441a421373efa01177
Stefan Battiston <stefan.battiston@gmail.com>
2019-10-17 22:38:43 -0300

    Functional benchmarks for serialization methods.

--- benches/clustering.rs
+++ benches/clustering.rs
@@ -17,7 +17,7 @@ fn clustering_benchmark(c: &mut Criterion) {
 
   for (zoom, clusters) in &[(7,1), (8,6), (9,16), (10,51), (11,162)] {
     clustering.throughput(Throughput::Elements((sample_markers.len() / clusters) as u64));
-    clustering.bench_with_input(BenchmarkId::from_parameter(zoom), &zoom, |b, &zoom_cur| b.iter_batched_ref(
+    clustering.bench_with_input(BenchmarkId::from_parameter(zoom), &zoom, |b, &zoom_cur| b.iter_batched(
       || sample_markers.to_vec(),
       |mut markers| cluster_markers(black_box(&mut Vec::new()), black_box(&mut markers), &DEFAULT_BOUNDS, *zoom_cur, black_box(&config)),
       BatchSize::SmallInput
--- tests/serialize.rs
+++ tests/serialize.rs
@@ -2,28 +2,41 @@ extern crate wasm_bindgen;
 extern crate wasm_bindgen_test;
 extern crate utilities;
 extern crate webassembly_marker_clusterer;
+extern crate web_sys;
+extern crate easybench_wasm;
 
 use wasm_bindgen_test::*;
 use wasm_bindgen::prelude::JsValue;
-use utilities::{ get_sample_markers };
-// use webassembly_marker_clusterer::structs::marker::Marker;
+use utilities::{ get_sample_markers, get_unique_sample_markers };
+use webassembly_marker_clusterer::structs::marker::Marker;
+use web_sys::console;
 
 // This runs a unit test in the browser, so it can use browser APIs.
-// wasm_bindgen_test_configure!(run_in_browser);
+wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn serialize_deserialize() {
-  let markers = get_sample_markers();
-  // let js_value = JsValue::from_serde(&markers).unwrap();
+  let unique_sample_markers = get_unique_sample_markers();
+  let run_time = 10.0;
 
-  // microbench::bench(&options, "serialize/serde-default", || JsValue::from_serde(&markers).unwrap());
-  // microbench::bench(&options, "deserialize/serde-default", || -> Vec<Marker> { js_value.into_serde().unwrap() });
+  let serialize_default_serde = easybench_wasm::bench_limit(run_time,
+    || JsValue::from_serde(&unique_sample_markers).unwrap());
+  let serialize_serde_wasm_bindgen = easybench_wasm::bench_limit(run_time,
+    || serde_wasm_bindgen::to_value(&unique_sample_markers).unwrap());
 
-  // println!("reverse: {}", easybench::bench(|| JsValue::from_serde(&markers).unwrap()));
+  console::log_1(&format!("serialize/default-serde:      {}", serialize_default_serde).into());
+  console::log_1(&format!("serialize/serde-wasm-bindgen: {}", serialize_serde_wasm_bindgen).into());
 
-  // let bench_result = benchmarking::bench_function(move |measurer| {
-  //   measurer.measure(|| JsValue::from_serde(&markers).unwrap())
-  // }).unwrap();
+  let sample_markers = get_sample_markers();
 
-  // println!("{:?}", bench_result.elapsed());
+  let deserialize_default_serde = easybench_wasm::bench_env_limit(run_time,
+    JsValue::from_serde(&sample_markers).unwrap(),
+    |js_value| js_value.into_serde::<Vec<Marker>>().unwrap());
+
+  let deserialize_serde_wasm_bindgen = easybench_wasm::bench_env_limit(run_time,
+    JsValue::from_serde(&sample_markers).unwrap(),
+    |js_value| serde_wasm_bindgen::from_value::<Vec<Marker>>(js_value).unwrap());
+
+  console::log_1(&format!("deserialize/default-serde:      {}", deserialize_default_serde).into());
+  console::log_1(&format!("deserialize/serde-wasm-bindgen: {}", deserialize_serde_wasm_bindgen).into());
 }
